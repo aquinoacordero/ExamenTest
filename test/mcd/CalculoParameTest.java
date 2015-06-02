941f8bd1b48d2c7785da69fc28aa71830cfba087
aquinoacordero <aquinoacordero@atanasoft02.danielcastelao.org>
2015-06-02 11:27:43 +0200

    testParamet

--- test/mcd/CalculoParameTest.java
+++ test/mcd/CalculoParameTest.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package mcd;
 
 import org.junit.After;
@@ -16,33 +17,40 @@ import static org.junit.Assert.*;
  *
  * @author aquinoacordero
  */
-public class CalculoTest {
-
-
-    public CalculoTest() {
-
+public class CalculoParameTest {
+    
+    public CalculoParameTest(int num1, int num2) {
     }
-
+    
     @BeforeClass
     public static void setUpClass() {
     }
-
+    
     @AfterClass
     public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
     }
 
     
     @Test
     public void testObtenerMCD() {
         Calculo calc = new Calculo();
-        int num1 = 1;
-        int num2 = 1;
-        int expResult = 1;
-        int result = calc.obtenerMCD(num1, num2);
-        assertEquals(expResult, result, 0);
-        
-        System.out.println("numero1 :"+num1+" numero2 : "+num2+
-                " resultado= "+expResult);
+        System.out.println("obtenerMCD");
+        int num1 = 0;
+        int num2 = 0;
+        Calculo instance = new Calculo();
+        int expResult = 0;
+        int result = instance.obtenerMCD(num1, num2);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
-
+    
 }
